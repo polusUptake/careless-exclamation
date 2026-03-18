9742764d7fa0e4293f0d252285f40aa2f1b4554c
polusUptake <reuel14luis@gmail.com>
2026-03-18 19:40:45 +0530

    frontend and backend basic skeletons

---
+++ oops_backend/src/main/java/com/proj1/oops_backend/OopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proj1.oops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsBackendApplication.class, args);
+	}
+
+}
