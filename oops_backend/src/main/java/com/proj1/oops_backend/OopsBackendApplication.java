cdd8edeee8a6fcb5c65d379c0996d74b2995316d
polusUptake <reuel14luis@gmail.com>
2026-03-22 23:48:14 +0530

    backend test hello

--- oops_backend/src/main/java/com/proj1/oops_backend/OopsBackendApplication.java
+++ oops_backend/src/main/java/com/proj1/oops_backend/OopsBackendApplication.java
@@ -2,13 +2,20 @@ package com.proj1.oops_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class OopsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OopsBackendApplication.class, args);
-		
-	}
+ public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
