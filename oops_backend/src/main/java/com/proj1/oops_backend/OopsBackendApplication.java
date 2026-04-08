164e1a715075b6dc58f56e36f55c18fcab501199
polusUptake <reuel14luis@gmail.com>
2026-04-08 20:04:57 +0530

    removed hello test

--- oops_backend/src/main/java/com/proj1/oops_backend/OopsBackendApplication.java
+++ oops_backend/src/main/java/com/proj1/oops_backend/OopsBackendApplication.java
@@ -2,8 +2,6 @@ package com.proj1.oops_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,9 +11,4 @@ public class OopsBackendApplication {
  public static void main(String[] args) {
       SpringApplication.run(OopsBackendApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
-
 }
